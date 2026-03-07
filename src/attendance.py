8039ce74059eb144cc7660078e6db26acdd32a2e
haemin8886 <haemin8886@kyonggi.ac.kr>
2026-03-07 11:44:53 +0900

    issue 11 해결

--- src/attendance.py
+++ src/attendance.py
@@ -27,5 +27,5 @@ def mark_present(name):
 def get_daily_tuple():
     t = ("Monday", "Tuesday")
     # FIXME: AttributeError: 'tuple' object has no attribute 'append'
-    t.append("Wednesday")
+    t = t + ("Wednesday",)
     return t
